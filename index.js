ea3167855d4671ef352755f544a405c3b127898f
채준배_한국사회주택협회 <green@ksha.us>
2025-12-21 10:03:40 +0900

    Initialize React application with StyleFitApp component

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StyleFitApp from "./StyleFitApp.jsx";
+
+// root 요소 찾기
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// 컴포넌트 렌더링
+root.render(<StyleFitApp />);
