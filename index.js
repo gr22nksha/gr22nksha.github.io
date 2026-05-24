645933a64f694f23a86fec565f0ae9d8d30aaf8d
채준배_한국사회주택협회 <green@ksha.us>
2026-05-24 23:03:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import StyleFitApp from "./StyleFitApp.jsx";
-
+//import React from "react";
+// import ReactDOM from "react-dom/client";
+// import StyleFitApp from "./StyleFitApp.jsx";
+function StyleFitApp() {
 // root 요소 찾기
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
